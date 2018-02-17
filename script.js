1103a95137595e4ddd5fcaff203f4f5dfb45256b
Daniel A <audaniel9@gmail.com>
2018-02-17 14:19:02 -0500

    sidbar and more js

--- script.js
+++ script.js
@@ -7,18 +7,40 @@ function changeTheme() {
   var signin = document.getElementById("signinAccountBox");
   var personal = document.getElementById("personalInfo");
   var device = document.getElementById("deviceInfo");
+  var service = document.getElementById("servicesInfo");
 
-  if(create.className !== 'themeStyleAccount' && signin.className !== 'themeStyleAccount' &&
-      personal.className !== 'themeStyleInfo' && device.className !== 'themeStyleInfo') {
-    create.className = 'themeStyleAccount';
-    signin.className = 'themeStyleAccount';
-    personal.className = 'themeStyleInfo';
-    device.className = 'themeStyleInfo';
+  if(create.className !== "themeStyleAccount" && signin.className !== "themeStyleAccount" &&
+      personal.className !== "themeStyleInfo" && device.className !== "themeStyleInfo" &&
+      service.className !== "themeStyleInfo") {
+    create.className = "themeStyleAccount";
+    signin.className = "themeStyleAccount";
+    personal.className = "themeStyleInfo";
+    device.className = "themeStyleInfo";
+    service.className = "themeStyleInfo";
   }
   else {
-    create.className = 'accountArea';
-    signin.className = 'accountArea';
-    personal.className = 'formArea';
-    device.className = 'formArea';
+    create.className = "accountArea";
+    signin.className = "accountArea";
+    personal.className = "formArea";
+    device.className = "formArea";
+    service.className = "formArea";
   }
+}
+
+function menuDrop() {
+  var visible = document.getElementById("langDrop");
+  if(visible.style.display == "none") {
+    visible.style.display = "flex";
+  }
+  else {
+    visible.style.display = "none";
+  }
+}
+
+function changeLang() {
+  document.getElementById("createAnAccount").innerHTML = "Crea una Cuenta";
+  document.getElementById("signIn").innerHTML = "Registrarse";
+  document.getElementById("personalInformation").innerHTML = "Informacion Personal";
+  document.getElementById("deviceInformation").innerHTML = "Información del Dispositivo";
+  document.getElementById("services").innerHTML = "Servicios";
 }
