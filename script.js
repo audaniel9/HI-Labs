d1d6ed47f855a0a2d9d4b60a049072108cee6aa2
Daniel A <audaniel9@gmail.com>
2018-02-17 00:42:10 -0500

    more working js

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-<script>
-function submitForms() {
-    document.getElementById("form").submit();
+function submitInfoForm() {
+    document.getElementById("infoForm").submit();
 }
-</script>
+
+function changeTheme() {
+  var create = document.getElementById("createAccountBox");
+  var signin = document.getElementById("signinAccountBox");
+  var personal = document.getElementById("personalInfo");
+  var device = document.getElementById("deviceInfo");
+
+  if(create.className !== 'themeStyleAccount' && signin.className !== 'themeStyleAccount' &&
+      personal.className !== 'themeStyleInfo' && device.className !== 'themeStyleInfo') {
+    create.className = 'themeStyleAccount';
+    signin.className = 'themeStyleAccount';
+    personal.className = 'themeStyleInfo';
+    device.className = 'themeStyleInfo';
+  }
+  else {
+    create.className = 'accountArea';
+    signin.className = 'accountArea';
+    personal.className = 'formArea';
+    device.className = 'formArea';
+  }
+}
